f27a2a6428dab367a9286db1aa7dc227e24970c4
osmidhun <midhun.os@gmail.com>
2014-07-12 23:25:45 +0100

    initial Commit

---
+++ CheckOutProcessorTest/TestCheckOutProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
+using BusinessProcessor;
+
+namespace CheckOutProcessorTest
+{
+    [TestFixture]
+    public class TestCheckOutProcessor
+    {
+  
+
+        [Test]
+        public void Test_CheckOut_Method_Returns_Total_Of_SKU()
+        {
+            var classUnderTest = new CheckOutProcessor();
+            var total = classUnderTest.CheckOut("A", 50);
+          Assert.AreEqual(total,50);
+        }
+    }
+}
+
