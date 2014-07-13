163a4a84ef3e1a75f08194bb75fd46ffbca051fa
osmidhun <midhun.os@gmail.com>
2014-07-13 19:26:05 +0100

    Test SpecialOffer when three times SKU_A_CheckedOut 20 Offer-Failed

--- CheckOutProcessorTest/TestCheckOutProcessor.cs
+++ CheckOutProcessorTest/TestCheckOutProcessor.cs
@@ -19,7 +19,22 @@ namespace CheckOutProcessorTest
             Assert.AreEqual(50, total);
           total = classUnderTest.CheckOut("B", 60);
           Assert.AreEqual(110, total);
+          total = classUnderTest.CheckOut("C", 10);
+          Assert.AreEqual(120, total);
         }
+
+        [Test]
+        public void Test_SpecialOffer_When_Tree_Times_SKU_A_CheckedOut_20_Offer()
+        {
+            var classUnderTest = new CheckOutProcessor.CheckOutProcessor();
+            var total = classUnderTest.CheckOut("A", 50);
+            Assert.AreEqual(50, total);
+            total = classUnderTest.CheckOut("A", 50);
+            Assert.AreEqual(100, total);
+            total = classUnderTest.CheckOut("A", 50);
+            Assert.AreEqual(130, total);
+        }
+
     }
 }
 
